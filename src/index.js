957ee682495db31822a6c68bd3af19be24f4d942
Jordan Trickett <jordan1trickett@gmail.com>
2020-04-21 15:16:54 +0100

    redux intergrated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import App from "./components/App";
+import reducers from "./reducers";
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.querySelector("#root")
 );
